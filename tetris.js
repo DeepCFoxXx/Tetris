afa17a4f4d51cc3c482c5575abed26803bb1dc8b
DeepCFoxXx <learncode23@gmail.com>
2018-02-26 11:39:46 +0000

    draw board function done

--- tetris.js
+++ tetris.js
@@ -6,4 +6,16 @@ var height = 20;
 var tilez = 24;
 
 canvas.width = width * tilez;
-canvas.height = height * tilez; 
+canvas.height = height * tilez;
+
+
+function drawBoard() {
+  fs = ctxfillstyle;
+  for (var y = 0; y < height; y++) {
+    for (var x = 0; x < width; x++) {
+      ctxfillstyle = board[y][x] ? 'red' : 'white';
+      drawSquare(x, y, tilez, tilez);
+    }
+  }
+  ctxfillstyle = fs;
+}
