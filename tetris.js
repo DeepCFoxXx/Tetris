170ffe8d1d077e1c5e3295594f77b51ed6bc381d
DeepCFoxXx <learncode23@gmail.com>
2018-02-26 11:30:32 +0000

    started tetris.js

---
+++ tetris.js
@@ -0,0 +1,9 @@
+var canvas = document .getElementById('board');
+var ctx = canvas.getContext('2d');
+
+var width = 10;
+var height = 20;
+var tilez = 24;
+
+canvas.width = width * tilez;
+canvas.height = height * tilez; 
