c47b318766ccdf861ecb4555f0113e2c96bc7bcd
DeepCFoxXx <learncode23@gmail.com>
2018-02-28 10:49:26 +0000

    refactoring

--- public/tetris.js
+++ public/tetris.js
@@ -8,17 +8,13 @@ var tilesz = 24;
 canvas.width = width * tilesz;
 canvas.height = height * tilesz;
 
+// board constructor
 var board = [];
 for (var r = 0; r < height; r++) {
 	board[r] = [];
 	for (var c = 0; c < width; c++) {
 		board[r][c] = "";
 	}
-}
-
-function newPiece() {
-	var p = pieces[parseInt(Math.random() * pieces.length, 10)];
-	return new Piece(p[0], p[1]);
 }
 
 function drawSquare(x, y) {
@@ -31,6 +27,17 @@ function drawSquare(x, y) {
 	ctx.strokeStyle = ss;
 }
 
+function drawBoard() {
+	var fs = ctx.fillStyle;
+	for (var y = 0; y < height; y++) {
+		for (var x = 0; x < width; x++) {
+			ctx.fillStyle = board[y][x] || clear;
+			drawSquare(x, y, tilesz, tilesz);
+		}
+	}
+	ctx.fillStyle = fs;
+}
+
 function Piece(patterns, color) {
 	this.pattern = patterns[0];
 	this.patterns = patterns;
@@ -41,49 +48,6 @@ function Piece(patterns, color) {
 	this.x = width/2-parseInt(Math.ceil(this.pattern.length/2), 10);
 	this.y = -2;
 }
-
-Piece.prototype.rotate = function() {
-	var nudge = 0;
-	var nextpat = this.patterns[(this.patterni + 1) % this.patterns.length];
-
-	if (this._collides(0, 0, nextpat)) {
-		nudge = this.x > width / 2 ? -1 : 1;
-	}
-
-	if (!this._collides(nudge, 0, nextpat)) {
-		this.undraw();
-		this.x += nudge;
-		this.patterni = (this.patterni + 1) % this.patterns.length;
-		this.pattern = this.patterns[this.patterni];
-		this.draw();
-	}
-};
-
-var WALL = 1;
-var BLOCK = 2;
-Piece.prototype._collides = function(dx, dy, pat) {
-	for (var ix = 0; ix < pat.length; ix++) {
-		for (var iy = 0; iy < pat.length; iy++) {
-			if (!pat[ix][iy]) {
-				continue;
-			}
-
-			var x = this.x + ix + dx;
-			var y = this.y + iy + dy;
-			if (y >= height || x < 0 || x >= width) {
-				return WALL;
-			}
-			if (y < 0) {
-				continue;
-			}
-			if (board[y][x] !== "") {
-				return BLOCK;
-			}
-		}
-	}
-
-	return 0;
-};
 
 Piece.prototype.down = function() {
 	if (this._collides(0, 1, this.pattern)) {
@@ -112,6 +76,77 @@ Piece.prototype.moveLeft = function() {
 	}
 };
 
+Piece.prototype.rotate = function() {
+	var nudge = 0;
+	var nextpat = this.patterns[(this.patterni + 1) % this.patterns.length];
+
+	if (this._collides(0, 0, nextpat)) {
+		nudge = this.x > width / 2 ? -1 : 1;
+	}
+
+	if (!this._collides(nudge, 0, nextpat)) {
+		this.undraw();
+		this.x += nudge;
+		this.patterni = (this.patterni + 1) % this.patterns.length;
+		this.pattern = this.patterns[this.patterni];
+		this.draw();
+	}
+};
+
+Piece.prototype._fill = function(color) {
+	var fs = ctx.fillStyle;
+	ctx.fillStyle = color;
+	var x = this.x;
+	var y = this.y;
+	for (var ix = 0; ix < this.pattern.length; ix++) {
+		for (var iy = 0; iy < this.pattern.length; iy++) {
+			if (this.pattern[ix][iy]) {
+				drawSquare(x + ix, y + iy);
+			}
+		}
+	}
+	ctx.fillStyle = fs;
+};
+
+Piece.prototype.undraw = function(ctx) {
+	this._fill(clear);
+};
+
+Piece.prototype.draw = function(ctx) {
+	this._fill(this.color);
+};
+
+function newPiece() {
+	var p = pieces[parseInt(Math.random() * pieces.length, 10)];
+	return new Piece(p[0], p[1]);
+}
+
+var WALL = 1;
+var BLOCK = 2;
+Piece.prototype._collides = function(dx, dy, pat) {
+	for (var ix = 0; ix < pat.length; ix++) {
+		for (var iy = 0; iy < pat.length; iy++) {
+			if (!pat[ix][iy]) {
+				continue;
+			}
+
+			var x = this.x + ix + dx;
+			var y = this.y + iy + dy;
+			if (y >= height || x < 0 || x >= width) {
+				return WALL;
+			}
+			if (y < 0) {
+				continue;
+			}
+			if (board[y][x] !== "") {
+				return BLOCK;
+			}
+		}
+	}
+
+	return 0;
+};
+
 var lines = 0;
 var done = false;
 Piece.prototype.lock = function() {
@@ -122,7 +157,7 @@ Piece.prototype.lock = function() {
 			}
 
 			if (this.y + iy < 0) {
-				alert("You smell diffrent when you are awake");
+				alert("Game Finished");
 				done = true;
 				return;
 			}
@@ -154,29 +189,6 @@ Piece.prototype.lock = function() {
 		drawBoard();
 		linecount.textContent = "Lines: " + lines;
 	}
-};
-
-Piece.prototype._fill = function(color) {
-	var fs = ctx.fillStyle;
-	ctx.fillStyle = color;
-	var x = this.x;
-	var y = this.y;
-	for (var ix = 0; ix < this.pattern.length; ix++) {
-		for (var iy = 0; iy < this.pattern.length; iy++) {
-			if (this.pattern[ix][iy]) {
-				drawSquare(x + ix, y + iy);
-			}
-		}
-	}
-	ctx.fillStyle = fs;
-};
-
-Piece.prototype.undraw = function(ctx) {
-	this._fill(clear);
-};
-
-Piece.prototype.draw = function(ctx) {
-	this._fill(this.color);
 };
 
 var pieces = [
@@ -225,17 +237,6 @@ function key(k) {
 		piece.moveRight();
 		dropStart = Date.now();
 	}
-}
-
-function drawBoard() {
-	var fs = ctx.fillStyle;
-	for (var y = 0; y < height; y++) {
-		for (var x = 0; x < width; x++) {
-			ctx.fillStyle = board[y][x] || clear;
-			drawSquare(x, y, tilesz, tilesz);
-		}
-	}
-	ctx.fillStyle = fs;
 }
 
 function main() {
